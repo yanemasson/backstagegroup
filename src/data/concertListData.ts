e93ad4038e2a5c7ce81b00b976b685256b2e90f6
yanemasson <gluedboy@gmail.com>
2024-12-23 14:37:33 +0700

    hi

--- src/data/concertListData.ts
+++ src/data/concertListData.ts
@@ -6,7 +6,7 @@ export const ConcertListData: Concert[] = [
         city: 'Красноярск',
         location: 'Институт искусств имени Хворостовского',
         date: new Date(2025,0, 9, 19, 0),
-        poster: 'src/assets/image/posters/1.jpg',
+        poster: '/src/assets/image/posters/1.jpg',
         descriptionShort: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         descriptionFull: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
@@ -15,7 +15,7 @@ export const ConcertListData: Concert[] = [
         city: 'Ленинск-Кузнецкий',
         location: 'Зал МАУК «Дворец культуры и искусства»',
         date: new Date(2025,10,19,19,0),
-        poster: 'src/assets/image/posters/2.jpg',
+        poster: '/src/assets/image/posters/2.jpg',
         descriptionShort: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         descriptionFull: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
@@ -24,7 +24,7 @@ export const ConcertListData: Concert[] = [
         city: 'Междуреченск',
         location: 'ДК "Распадский"',
         date: new Date(2025,10,20,18,0),
-        poster: 'src/assets/image/posters/3.jpg',
+        poster: '/src/assets/image/posters/3.jpg',
         descriptionShort: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         descriptionFull: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
@@ -33,7 +33,7 @@ export const ConcertListData: Concert[] = [
         city: 'Новокузнецк',
         location: 'ДК «Алюминщик»',
         date: new Date(2025,10,21,19,0),
-        poster: 'src/assets/image/posters/4.jpg',
+        poster: '/src/assets/image/posters/4.jpg',
         descriptionShort: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         descriptionFull: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
