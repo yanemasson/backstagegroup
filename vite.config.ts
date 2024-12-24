2c681a3b11be11428b38bc5b92f46c4e88029c48
yanemasson <gluedboy@gmail.com>
2024-12-25 01:05:06 +0700

    add Not Found Page

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from 'vite'
 import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import react from '@vitejs/plugin-react'
 import svgr from "vite-plugin-svgr";
+import * as fs from 'fs-extra'
+import type { Plugin } from 'vite'
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), netlifyPlugin(), svgr()],
+  plugins: [react(
+      {
+          name: 'copy-redirects',
+          enforce: 'post',
+          async closeBundle() {
+              await fs.copy('public/_redirects', 'dist/_redirects')
+          }
+      } as Plugin
+  ), netlifyPlugin(), svgr()],
 })
