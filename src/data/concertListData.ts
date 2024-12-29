9b8e4a4ceeb5aca067aa04fff63b8edf40900159
yanemasson <gluedboy@gmail.com>
2024-12-29 19:51:50 +0700

    add videos on decap cms

--- src/data/concertListData.ts
+++ src/data/concertListData.ts
@@ -1,14 +1,9 @@
 
 /*export const ConcertListData: Concert[] = [
     {
-        city: 'Красноярск',
-        location: 'Институт искусств имени Хворостовского',
-        date: new Date(2025,0, 9, 19, 0),
-        url: 'https://kras.kassy.events/events/koncerty-i-shou/2-14475/',
+ date: new Date(2025,0, 9, 19, 0),
         poster: '/image/posters/1.jpg',
         videos: ['/videos/promo_video.mp4'],
-        descriptionShort: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        descriptionFull: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     },
 
 
