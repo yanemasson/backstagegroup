3691bf1cafb7aa1eb10ef8e1fa6479f88db40b9f
yanemasson <gluedboy@gmail.com>
2025-01-24 15:48:42 +0700

    fix date error

--- src/hooks/useDate.ts
+++ src/hooks/useDate.ts
@@ -4,5 +4,5 @@ const months = ['января', 'февраля', 'марта', 'апреля', 
 export const useDate = (date:string)=> {
     const dateArr = date.split(' ')[0].split('-')
     const time = date.split(' ')[1]
-    return Number(dateArr[2]) + ' ' + months[Number(dateArr[1])] + ' в ' + time
+    return Number(dateArr[2]) + ' ' + months[Number(dateArr[1]) - 1] + ' в ' + time
 }
