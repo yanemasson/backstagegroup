a69d66e8ff0a32a9fa9b1ad6f5521b9b8d925268
yanemasson <gluedboy@gmail.com>
2025-01-24 14:13:20 +0700

    add to concerts button and change ConcertCard.tsx on mobile

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,14 +1,21 @@
 import Text, {TextVariant} from "../../components/Text.tsx";
 import Button, {ButtonVariant} from "../../components/Button.tsx";
 import {Link} from "react-router";
+import {SEO} from "../../components/SEO.tsx";
 
 const NotFoundPage = () => {
     return (
-        <div className=' flex flex-col items-center justify-center gap-5 h-screen text-9xl text-white font-extrabold bg-black'>
-            404
-            <Text variant={TextVariant.P}>Такой страницы не существует</Text>
-            <Link to={'/'}><Button variant={ButtonVariant.outline}><Text variant={TextVariant.P}>Главная</Text></Button></Link>
-        </div>
+        <>
+            <SEO title={'404 | Страница не найдена'}
+                 description={"Билеты на лучшие балетные спектакли и симфонические концерты." +
+                "Классическая музыка, премьеры в Вашем городе"}/>
+            <div className=' flex flex-col items-center justify-center gap-5 h-screen text-9xl text-white font-extrabold bg-black'>
+                404
+                <Text variant={TextVariant.P}>Такой страницы не существует</Text>
+                <Link to={'/'}><Button variant={ButtonVariant.outline}><Text variant={TextVariant.P}>Главная</Text></Button></Link>
+            </div>
+        </>
+
     );
 };
 
