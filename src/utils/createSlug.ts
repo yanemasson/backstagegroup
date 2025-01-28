6c385084b3526ffb42a735926aac5db969f8edab
yanemasson <gluedboy@gmail.com>
2025-01-28 22:03:56 +0700

    fix some lil bugs

--- src/utils/createSlug.ts
+++ src/utils/createSlug.ts
@@ -1,7 +1,7 @@
-const createSlug = (title: string, city: string, date:string) => {
+const createSlug = (title: string, city: string, date: string) => {
     return `${title}_${city}_${date}`
         .toLowerCase()
-        .replace(/[^a-zа-яё0-9-\s]/g, '')
+        .replace(/[^a-zа-яё0-9-_\s]/g, '') // добавили _ в список разрешенных символов
         .replace(/\s+/g, '_')
         .replace(/-+/g, '_');
 };
