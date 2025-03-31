4e2bf214a1ffcaad8e60b3097b64e5be49a02241
yanemasson <gluedboy@gmail.com>
2025-03-31 23:07:10 +0700

    Removed unused components, hooks and context

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import Navbar from "./layouts/Navbar/Navbar.tsx";
 import Footer from "./layouts/Footer/Footer.tsx";
 import {BrowserRouter} from "react-router";
-import {CityProvider} from "./contexts/CityContext.tsx";
 import { HelmetProvider } from 'react-helmet-async';
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import AppRoutes from "./components/AppRoutes.tsx";
@@ -11,20 +10,18 @@ function App() {
   return (
       <HelmetProvider>
           <MetricsProvider>
-              <CityProvider>
-                  <BrowserRouter>
-                      <ScrollToTop />
-                      <div className='bg-darkgray text-white flex flex-col justify-center items-center'>
-                          <div className="flex flex-col items-center min-h-screen w-[90vw] xl:w-[1166px] ">
-                              <Navbar/>
-                              <main className="flex-grow py-[64px] xl:py-[84px]">
-                                  <AppRoutes/>
-                              </main>
-                              <Footer/>
-                          </div>
+              <BrowserRouter>
+                  <ScrollToTop />
+                  <div className='bg-darkgray text-white flex flex-col justify-center items-center'>
+                      <div className="flex flex-col items-center min-h-screen w-[90vw] xl:w-[1166px] ">
+                          <Navbar/>
+                          <main className="flex-grow py-[64px] xl:py-[84px]">
+                              <AppRoutes/>
+                          </main>
+                          <Footer/>
                       </div>
-                  </BrowserRouter>
-              </CityProvider>
+                  </div>
+              </BrowserRouter>
           </MetricsProvider>
       </HelmetProvider>
 
