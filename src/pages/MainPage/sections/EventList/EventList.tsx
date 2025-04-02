ea678aaa88325e91585f48ed5ce3f4b3a1aceacd
yanemasson <gluedboy@gmail.com>
2025-04-02 13:19:32 +0700

    refactor: remove unused city detection logic and add keys to EventCard elements in EventList section

--- src/pages/MainPage/sections/EventList/EventList.tsx
+++ src/pages/MainPage/sections/EventList/EventList.tsx
@@ -8,12 +8,6 @@ import EventCardMobile from "../../../../components/EventCard/EventCardMobile.ts
 import createSlug from "../../../../utils/createSlug.ts";
 
 const EventList = () => {
-/*
-    const { selectedCity } = useCity();
-    const selectedCity = 'Красноярск'
-    const filteredEvents = events.filter(item => item.city === selectedCity);
-
-*/
     const {events} = useEvents()
     const xl = useMediaBreakpoint('xl')
     return (
@@ -22,13 +16,11 @@ const EventList = () => {
                 events.map((item, index) => (
                     xl
                         ? <EventCardDesktop key={index} item={item} to={createSlug(item.eventId)}/>
-                        : <EventCardMobile item={item} to={createSlug(item.eventId)}/>
+                        : <EventCardMobile key={index} item={item} to={createSlug(item.eventId)}/>
                     )
                 )
             ) : (
                 <div className="flex flex-col items-center justify-center gap-5 py-20">
-                    {/*<Text variant={TextVariant.B}>В городе {selectedCity} пока нет концертов</Text>
-                    <Text variant={TextVariant.P}>Выберите другой город или следите за обновлениями</Text>*/}
                     <Link to={'events'}>
                         <Button variant={ButtonVariant.outline}>
                             <Text variant={TextVariant.P}>Все концерты</Text>
