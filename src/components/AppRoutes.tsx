d2eff9661d706badd3c5f5b6a5d06982cf21a699
yanemasson <gluedboy@gmail.com>
2025-04-04 22:48:44 +0700

    refactor: remove unused page

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -3,7 +3,6 @@ import LoadingSpinner from "./LoadingSpinner.tsx";
 import {Route, Routes} from "react-router";
 
 const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
-const ConcertListPage = lazy(() => import('../pages/EventListPage/EventListPage'));
 const EventPage = lazy(() => import('../pages/EventPage/EventPage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
 const RefundPage = lazy(() => import('../pages/RefundPage/RefundPage.tsx'))
@@ -14,7 +13,6 @@ const AppRoutes = () => {
         <Suspense fallback={<LoadingSpinner />}>
             <Routes>
                 <Route index element={<MainPage/>}/>
-                <Route path={'events'} element={<ConcertListPage/>} />
                 <Route path={'events/:id'} element={<EventPage/>} />
                 <Route path={'refund'} element={<RefundPage/>}/>
                 <Route path={'*'} element={<NotFoundPage />} />
