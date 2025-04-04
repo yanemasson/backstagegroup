eac00e2d16391c2711dd72ee5516032fbb282204
yanemasson <gluedboy@gmail.com>
2025-04-05 01:19:29 +0700

    feat: Updated LoadingSpinner component styles for better visual feedback during video loading

--- src/components/LoadingSpinner.tsx
+++ src/components/LoadingSpinner.tsx
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <div className="animate-spin rounded-full h-16 w-16 border-solid border-4 border-b-yellow  border-white" />
+            <div className="animate-spin rounded-full h-16 w-16 border-solid border-4 border-b-light-brown  border-white" />
         </div>
     );
 };
