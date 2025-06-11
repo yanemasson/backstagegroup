d1497878dd69a7290b7601ec2b08a894813cea23
yanemasson <gluedboy@gmail.com>
2025-06-11 12:15:20 +0700

    feat: add new pages to AppRoutes

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -5,7 +5,9 @@ import {Route, Routes} from "react-router";
 const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
 const EventPage = lazy(() => import('../pages/EventPage/EventPage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
-const RefundPage = lazy(() => import('../pages/RefundPage/RefundPage.tsx'))
+const RefundPage = lazy(() => import('../pages/RefundPage/RefundPage'))
+const NewsListPage = lazy(() => import('../pages/NewsListPage/NewsListPage'));
+const NewsPage = lazy(() => import('../pages/NewsPage/NewsPage'));
 // const AboutPage = lazy(() => import('./pages/AboutPage/AboutPage'))
 
 const AppRoutes = () => {
@@ -14,6 +16,8 @@ const AppRoutes = () => {
             <Routes>
                 <Route index element={<MainPage/>}/>
                 <Route path={'events/:id'} element={<EventPage/>} />
+                <Route path={'news'} element={<NewsListPage/>} />
+                <Route path={'news/:id'} element={<NewsPage/>} />
                 <Route path={'refund'} element={<RefundPage/>}/>
                 <Route path={'*'} element={<NotFoundPage />} />
             </Routes>
