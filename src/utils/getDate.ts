ff6630bbc098670531fa4b8de3d474f7752ba7f0
gluedboy <gluedboy@gmail.com>
2025-07-02 20:00:27 +0700

    Обновлена логика функции getDate

--- src/utils/getDate.ts
+++ src/utils/getDate.ts
@@ -5,12 +5,19 @@ export const getDate = (datetime: string) => {
     const date = datetime.split('-')
     date[3] = date[2].split(/[ T]/)[1]
     date[2] = date[2].split(/[ T]/)[0]
+
+    let formattedTime = '';
+    if (date[3]) {
+        formattedTime = date[3].split(':').slice(0, 2).join(':');
+    }
+
+
     return {
         year: date[0],
         monthNum: date[1],
         monthStr: months[Number(date[1]) - 1],
         day: date[2],
-        time: date[3],
+        time: formattedTime,
         get formattedDate() {
             return this.day + '.' + this.monthNum + '.' + this.year;
         }
