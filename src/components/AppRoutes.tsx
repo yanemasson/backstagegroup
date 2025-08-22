730dfba721dccc082cc58c33fd6e62a2d92c2dcd
gluedboy <gluedboy@gmail.com>
2025-08-22 16:52:19 +0700

    Добавлены страницы для пользователського соглашения, публичной оферты и политики конфиденциальности

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -4,13 +4,17 @@ import {Route, Routes} from "react-router";
 
 const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
 const EventPage = lazy(() => import('../pages/EventPage/EventPage'));
-const EventsPage = lazy(() => import('../pages/EventsPage/EventsPage'));
-const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
-const RefundPage = lazy(() => import('../pages/RefundPage/RefundPage'))
 const NewsListPage = lazy(() => import('../pages/NewsListPage/NewsListPage'));
 const NewsPage = lazy(() => import('../pages/NewsPage/NewsPage'));
+
+const RefundPage = lazy(() => import('../pages/RefundPage/RefundPage'))
+const PrivacyPage = lazy(() => import('../pages/PrivacyPage/PrivacyPage'));
+const OfferPage = lazy(() => import('../pages/OfferPage/OfferPage'));
+const AgreementPage = lazy(() => import('../pages/AgreementPage/AgreementPage'));
+
 const PostponementPage = lazy(() => import('../pages/PostponementPage/PostponementPage'));
-// const AboutPage = lazy(() => import('./pages/AboutPage/AboutPage'))
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
+
 
 const AppRoutes = () => {
     return (
@@ -18,12 +22,16 @@ const AppRoutes = () => {
             <Routes>
                 <Route index element={<MainPage/>}/>
                 <Route path={'events/:id'} element={<EventPage/>} />
-                <Route path={'events'} element={<EventsPage/>} />
                 <Route path={'news'} element={<NewsListPage/>} />
                 <Route path={'news/:id'} element={<NewsPage/>} />
-                <Route path={'refund'} element={<RefundPage/>}/>
+
+                <Route path={'/user_agreement'} element={<AgreementPage/>} />
+                <Route path={'/offer'} element={<OfferPage/>} />
+                <Route path={'refund'} element={<RefundPage/>} />
+                <Route path={'privacy_policy'} element={<PrivacyPage/>} />
+
+                <Route path={'events/2406411'} element={<PostponementPage/>} />
                 <Route path={'*'} element={<NotFoundPage />} />
-                <Route path={'events/2406411'} element={<PostponementPage/>}/>
             </Routes>
         </Suspense>
     );
