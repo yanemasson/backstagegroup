efe7ef70520d392e76248458d4ad8ebf69237852
gluedboy <gluedboy@gmail.com>
2025-09-15 19:16:25 +0700

    Добавлена FAQ секция на главную страницу

--- src/pages/MainPage/sections/FAQ/FAQ.tsx
+++ src/pages/MainPage/sections/FAQ/FAQ.tsx
@@ -1,45 +1,76 @@
 import Text, {TextVariant} from '../../../../components/Text'
 import QuestionCard from "../../components/QuestionCard.tsx";
+import Anchor from "../../../../components/Anchor.tsx";
 
 const Faq = () => {
 
     const faqArr = [
         {
-            answer: 'Можно ли вести фото и видео съёмку во время концерта?',
-            question: 'text'
+            question: 'Можно ли вести фото и видео съёмку во время концерта?',
+            answer:
+                <>
+                    Да, вы можете вести любительскую фото и видеосъёмку на мобильные телефоны и небольшие камеры для личного пользования. Мы будем рады, если вы отметите нас в соцсетях! Профессиональная съёмка со вспышкой, штативами и большой оптикой запрещена.
+                </>
         },
         {
-            answer: 'Можно ли приходить на концерт с детьми?',
-            question: 'Концечно! Детям очень понравится музыка.' +
-                'Для вашего удобство на всех программах стоит возрастной рейтинг,' +
-                'чтобы вы приняли решение взять с собой детей или провести время наедине со своей второй половинкой.'
+            question: 'Можно ли купить билет по Пушкинской карте?',
+            answer:
+                <>
+                    К сожалению, мы не принимаем к оплате Пушкинскую карту. Но у нас всегда есть билеты в разных ценовых категориях, чтобы каждый мог найти вариант по своему бюджету.
+                </>
         },
         {
-            answer: 'Можно ли проносить свою еду и напитки?',
-            question: 'text'
+            question: 'Нужно ли распечатывать билет?',
+            answer:
+                <>
+                    Нет, распечатывать билет необязательно. Достаточно показать QR-код из письма, которое пришло на вашу электронную почту после покупки.
+                </>
         },
         {
-            answer: 'Можно ли вернуть купленный билет?',
-            question: 'text'
+            question: 'Где посмотреть фотографии с концерта?',
+            answer:
+                <>
+                    Все фотографии мы загружаем в альбомы в нашей <Anchor href={'https://vk.com/backstagegroup'}> группе ВКонтакте </Anchor> через неделю после завершения концерта. Следите за новостями!
+                </>
         },
         {
-            answer: 'Как с вами связаться?',
-            question: 'text'
+            question: 'Как с вами связаться?',
+            answer:
+                <div className='flex flex-col'>
+                    <p>Вы можете написать нам в сообщения</p>
+                    <p><Anchor href={'https://vk.com/backstagegroup'}>группы ВКонтакте</Anchor>,</p>
+                    <p><Anchor href={'https://t.me/backstagegroup24'}>Telegram-канал</Anchor>,</p>
+                    <Anchor href={'https://t.me/backstagegroup24'}>WhatsApp</Anchor>
+                    <p>или позвонить по <Anchor href={'tel:+79994400249'}>+7 999 440-02-49</Anchor>, <Anchor href={'tel:+79232157634'}>+7 923 215-76-34</Anchor></p>
+                </div>
         },
         {
-            answer: 'Где и когда можно будет найти фотографии с концерта?',
-            question: 'text'
+            question: 'Что делать, если не пришел билет?',
+            answer:
+                <div className='flex flex-col'>
+                    <p>Если вы купили билет на нашем сайте, и он не пришел Вам на почту, напишите</p>
+                    <Anchor href={'mailto:help@radario.ru'}>help@radario.ru</Anchor>
+                    <p>Так же можете написать нам в <Anchor href={'https://vk.com/backstagegroup'}>группу Вконтакте</Anchor>,</p>
+                    <p>или позвонить по <Anchor href={'tel:+79994400249'}>+7 999 440-02-49</Anchor>, <Anchor href={'tel:+79232157634'}>+7 923 215-76-34</Anchor></p>
+                </div>
+        },
+        {
+            question: 'Как вернуть билет?',
+            answer:
+                <>
+                    Подробную информацию, по возврату билетов, смотрите <Anchor href={'/refund'}>здесь</Anchor>
+                </>
         },
     ]
 
     return (
-        <section className='flex flex-col gap-[62px] w-full mb-[120px]'>
+        <section className='flex flex-col gap-[62px] w-[90vw] md:w-[1166px] mb-[120px]'>
             <Text variant={TextVariant.H2}>ОТВЕТЫ НА ЧАСТЫЕ ВОПРОСЫ</Text>
             <div className='flex flex-col gap-[30px] md:px-[30px]'>
                 {faqArr.map((item, index) => (
                     <QuestionCard key={item.question}
-                                  question={item.answer}
-                                  answer={item.question}
+                                  question={item.question}
+                                  answer={item.answer}
                                   isLast={index === faqArr.length - 1}
                     />
                 ))}
