384c7c1ac3be07acce407575326f2bfe961873af
gluedboy <gluedboy@gmail.com>
2025-09-15 19:16:57 +0700

    Добавлена FAQ секция на главную страницу

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,32 +1,30 @@
-import Hero from "./sections/Hero/Hero.tsx";
 import {SEO} from "../../components/SEO.tsx";
 import EventList from "./sections/EventList/EventList.tsx";
 import {lazy, Suspense} from "react";
 import LoadingSpinner from "../../components/LoadingSpinner.tsx";
-import Faq from "./sections/FAQ/FAQ.tsx";
-import NewsSection from "./sections/News/NewsSection.tsx";
 
+const Hero = lazy(() => import('./sections/Hero/Hero'));
 const AboutUsSection = lazy(() => import('../EventPage/sections/AboutUsSection'));
 const ReviewsSection = lazy(() => import('../EventPage/sections/ReviewsSection'));
-
+const NewsSection = lazy(() => import('./sections/News/NewsSection'));
+const Faq = lazy(() => import('./sections/FAQ/FAQ'));
 
 const MainPage = () => {
     return (
-        <div className='flex flex-col items-center overflow-hidden'>
+        <div className='flex flex-col items-center '>
             <SEO
                 title="Главная страница | Бэкстейдж, афиша, концерт, билеты"
                 description={"Билеты на лучшие балетные спектакли и симфонические концерты." +
                     "Классическая музыка, премьеры в Вашем городе"}
                 keywords="балет, симфонический оркестр, концерты, классическая музыка, билеты, афиша"
             />
-            <Hero/>
-            <div className='flex flex-col items-center gap-[100px] xl:gap-40'>
+            <div className='flex flex-col items-center gap-[100px] xl:gap-30'>
+                <Suspense fallback={<LoadingSpinner />}><Hero/></Suspense>
                 <Suspense fallback={<LoadingSpinner />}><EventList/></Suspense>
                 <Suspense fallback={<LoadingSpinner />}><AboutUsSection/></Suspense>
+                <Suspense fallback={<LoadingSpinner />}><ReviewsSection/></Suspense>
                 <Suspense fallback={<LoadingSpinner />}><NewsSection/></Suspense>
                 <Suspense fallback={<LoadingSpinner />}><Faq/></Suspense>
-
-                <Suspense fallback={<LoadingSpinner />}><ReviewsSection/></Suspense>
 
             </div>
         </div>
