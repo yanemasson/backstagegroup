380965be7d81f6a1a30aa079271f18d4f75ed2f6
gluedboy <gluedboy@gmail.com>
2025-11-26 14:29:18 +0700

    Изменен дизайн секции Location

--- src/pages/EventPage/sections/LocationSection.tsx
+++ src/pages/EventPage/sections/LocationSection.tsx
@@ -1,14 +1,12 @@
 import Text, {TextVariant} from "../../../components/Text.tsx";
-import TicketButton from "../../../components/Buttons/TicketButton.tsx";
 
 interface LocationSectionProps {
     location: string;
     address: string;
-    eventId: number;
     photos: string[];
 }
 
-const LocationSection = ({location, address, eventId, photos}: LocationSectionProps) => {
+const LocationSection = ({location, address, photos}: LocationSectionProps) => {
 
     if(photos.length === 0) {
         return <Text className='text-lightgray' variant={TextVariant.CAPTION}>
@@ -17,20 +15,17 @@ const LocationSection = ({location, address, eventId, photos}: LocationSectionPr
     }
 
     return (
-        <section id='location' className='flex flex-col gap-10 xl:gap-[52px]'>
+        <section id='location' className='flex flex-col gap-10 xl:gap-[60px]'>
             <Text variant={TextVariant.H2}>ПЛОЩАДКА</Text>
-            <div className='flex flex-col gap-[23px]'>
-                <div className='flex justify-between '>
+            <div className='flex flex-col gap-[30px] xl:gap-10'>
+                <div className='flex flex-col gap-2'>
+                    <Text variant={TextVariant.H4}>{location}</Text>
+                    <Text className='text-lightgray' variant={TextVariant.P}>{address}</Text>
+                </div>
+                <div className='flex flex-col lg:flex-row justify-between gap-2.5'>
                     {photos.map((photo) => (
                         <img className='md:w-[49%]' src={photo} alt={photo} key={photo} />
                     ))}
-                </div>
-                <div className='flex flex-col gap-10 xl:gap-[50px] w-[284px]'>
-                    <div className='flex flex-col gap-2.5 w-3/4 xl:w-full'>
-                        <Text className='text-light-brown leading-none' variant={TextVariant.H4}>{location}</Text>
-                        <Text className='' variant={TextVariant.P}>{address}</Text>
-                    </div>
-                    <TicketButton className='w-[90vw] h-[45px] md:w-[284px] md:h-[53px]' eventId={eventId}/>
                 </div>
             </div>
         </section>
