ccdc76629a6ab59f265d5f1eb108f4ab86462df0
gluedboy <gluedboy@gmail.com>
2025-11-26 14:48:19 +0700

    Изменен стиль компонента App

--- src/App.tsx
+++ src/App.tsx
@@ -5,27 +5,29 @@ import { HelmetProvider } from 'react-helmet-async';
 import ScrollToTop from "./components/ScrollToTop.tsx";
 import AppRoutes from "./components/AppRoutes.tsx";
 import MetricsProvider from "./components/Metrika/MetricsProvider.tsx";
+import {CityProvider} from "./context/CityContext.tsx";
 
 function App() {
 
-
     return (
         <HelmetProvider>
-            <BrowserRouter>
-                <MetricsProvider>
-                    <ScrollToTop />
-                    <div className='bg-darkgray text-white flex flex-col justify-center items-center'>
-                        <div className="flex flex-col items-center min-h-screen w-[90vw] xl:w-[1166px] ">
-                            <Navbar/>
-                            <main className="flex-grow py-[64px] xl:py-[84px]">
-                                <AppRoutes/>
-                            </main>
-                            <Footer/>
+            <CityProvider>
+                <BrowserRouter>
+                    <MetricsProvider>
+                        <ScrollToTop />
+                        <div className='bg-darkgray text-white flex flex-col justify-center items-center'>
+                            <div className="flex flex-col items-center min-h-screen w-[90vw] xl:w-[1166px] ">
+                                <Navbar/>
+                                <main className="flex-grow md:mt-[134px] mt-[118px] mb-20 ">
+                                    <AppRoutes/>
+                                </main>
+                                <Footer/>
+                            </div>
                         </div>
-                    </div>
-                </MetricsProvider>
-            </BrowserRouter>
-      </HelmetProvider>
+                    </MetricsProvider>
+                </BrowserRouter>
+            </CityProvider>
+        </HelmetProvider>
   )
 }
 
