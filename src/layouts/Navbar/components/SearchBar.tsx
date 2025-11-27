86f75e8ce7da68ca9197996a06d06351f52b4f33
gluedboy <gluedboy@gmail.com>
2025-11-27 14:51:44 +0700

    Подготовка к реализации SearchBar и Gallery

--- src/layouts/Navbar/components/SearchBar.tsx
+++ src/layouts/Navbar/components/SearchBar.tsx
@@ -1,7 +1,7 @@
 
 const SearchBar = () => {
     return (
-        <div className=' bg-semi-darkgray min-w-[172px] w-full md:w-[736px] md:h-[53px] h-10 p-2 md:p-4  text-dark-text'>
+        <div className=' bg-semi-darkgray min-w-[130px] w-full md:w-[736px] md:h-[53px] h-10 p-2 md:p-4 text-dark-text'>
 
         </div>
     );
