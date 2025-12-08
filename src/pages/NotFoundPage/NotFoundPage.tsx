e34e48d217340d566d9139830a6db07c8889a04e
gluedboy <gluedboy@gmail.com>
2025-12-08 22:11:33 +0700

    Обновлена NotFoundPage

--- src/pages/NotFoundPage/NotFoundPage.tsx
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -1,5 +1,5 @@
 import Text, {TextVariant} from "../../components/Text.tsx";
-import Button, {ButtonVariant} from "../../components/Buttons/Button.tsx";
+import Button, {ButtonSize, ButtonVariant} from "../../components/Buttons/Button.tsx";
 import {Link} from "react-router";
 import {SEO} from "../../components/SEO.tsx";
 
@@ -9,12 +9,12 @@ const NotFoundPage = () => {
             <SEO title={'404 | Страница не найдена'}
                  description={"Билеты на лучшие балетные спектакли и симфонические концерты." +
                 "Классическая музыка, премьеры в Вашем городе"}/>
-            <div className='  flex flex-col items-center justify-center gap-5 h-screen text-9xl font-extrabold '>
-                <div className='text-light-brown'>404</div>
-                <Text className='text-lightgray' variant={TextVariant.P}>Такой страницы не существует</Text>
+            <div className='flex flex-col items-center justify-center gap-5 h-screen text-9xl font-extrabold '>
+                <div className='text-text-accent'>404</div>
+                <Text className='text-text-secondary' variant={TextVariant.Body_L}>Такой страницы не существует</Text>
                 <Link to={'/'}>
-                    <Button className='h-[45px] w-[200px]' variant={ButtonVariant.outline}>
-                        <Text variant={TextVariant.P}>Главная</Text>
+                    <Button className='h-[45px] w-[200px]' size={ButtonSize.large} variant={ButtonVariant.secondary}>
+                        Главная
                     </Button>
                 </Link>
             </div>
