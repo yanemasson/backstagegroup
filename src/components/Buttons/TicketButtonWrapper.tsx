d28481a43ae37de3641465c2726f21cd39693adc
gluedboy <gluedboy@gmail.com>
2025-12-09 21:33:31 +0700

    Wrappers для Inticket и Radario объединены в один компонент

--- src/components/Buttons/TicketButtonWrapper.tsx
+++ src/components/Buttons/TicketButtonWrapper.tsx
@@ -4,18 +4,21 @@ interface TicketButtonWrapperProps {
     eventId: number;
     children: ReactNode;
     className?: string;
-    variant?: string;
 }
 
-const TicketButtonWrapper: FC<TicketButtonWrapperProps> = ({eventId, children}) => {
+const TicketButtonWrapper: FC<TicketButtonWrapperProps> = ({eventId, children, className}) => {
+    const eventIdString = eventId.toString();
+    const isIntickets = eventIdString.length > 7;
 
+    // Логика для Radario
     useEffect(() => {
+        if (isIntickets) return;
+
         if (!document.getElementById('radario-script')) {
             const script = document.createElement('script');
             script.id = 'radario-script';
             script.src = 'https://radario.ru/frontend/src/api/openapi/openapi.js';
             script.async = true;
-
             script.onload = () => {
                 if (window.radario) {
                     window.radario.Widgets.Event({
@@ -24,17 +27,37 @@ const TicketButtonWrapper: FC<TicketButtonWrapperProps> = ({eventId, children}) 
                         },
                         standalone: false,
                         createButton: false,
-                        eventId
+                        eventId: eventIdString
                     });
                 }
             };
             document.head.appendChild(script);
         }
-    }, [eventId]);
+    }, [eventIdString, isIntickets]);
 
+    const getInticketsUrl = () => {
+        if (eventIdString === '62738053') {
+            return `https://iframeab-pre6263.intickets.ru/seance/${eventIdString}/#abiframe`;
+        }
+        return `https://iframeab-pre11666.intickets.ru/seance/${eventIdString}/#abiframe`;
+    };
+
+    const href = isIntickets
+        ? getInticketsUrl()
+        : `#event/${eventIdString}`;
+
+    const linkProps = isIntickets
+        ? { rel: "noopener noreferrer" as const }
+        : {};
 
     return (
-        <a className='w-fit' href={`#event/${eventId}`}>{children}</a>
+        <a
+            className={`w-fit ${className}`}
+            href={href}
+            {...linkProps}
+        >
+            {children}
+        </a>
     );
 };
 
