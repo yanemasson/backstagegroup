495a9635e033bed1a82924c4ef692305c0748ff4
gluedboy <gluedboy@gmail.com>
2025-12-15 12:39:30 +0700

    Добавлена страница Events

--- src/components/AppRoutes.tsx
+++ src/components/AppRoutes.tsx
@@ -3,16 +3,18 @@ import LoadingSpinner from "./LoadingSpinner.tsx";
 import {Route, Routes} from "react-router";
 
 const MainPage = lazy(() => import('../pages/MainPage/MainPage'));
+
 const EventPage = lazy(() => import('../pages/EventPage/EventPage'));
+const EventsPage = lazy(() => import('../pages/EventsPage/EventsPage'));
+
 const NewsListPage = lazy(() => import('../pages/NewsListPage/NewsListPage'));
 const NewsPage = lazy(() => import('../pages/NewsPage/NewsPage'));
 
-const RefundPage = lazy(() => import('../pages/RefundPage/RefundPage'))
-const PrivacyPage = lazy(() => import('../pages/PrivacyPage/PrivacyPage'));
-const OfferPage = lazy(() => import('../pages/OfferPage/OfferPage'));
 const AgreementPage = lazy(() => import('../pages/AgreementPage/AgreementPage'));
+const OfferPage = lazy(() => import('../pages/OfferPage/OfferPage'));
+const PrivacyPage = lazy(() => import('../pages/PrivacyPage/PrivacyPage'));
+const RefundPage = lazy(() => import('../pages/RefundPage/RefundPage'))
 
-const PostponementPage = lazy(() => import('../pages/PostponementPage/PostponementPage'));
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
 
 
@@ -21,16 +23,18 @@ const AppRoutes = () => {
         <Suspense fallback={<LoadingSpinner />}>
             <Routes>
                 <Route index element={<MainPage/>}/>
+
+                <Route path={'events'} element={<EventsPage/>} />
                 <Route path={'events/:id'} element={<EventPage/>} />
+
                 <Route path={'news'} element={<NewsListPage/>} />
                 <Route path={'news/:id'} element={<NewsPage/>} />
 
                 <Route path={'user_agreement'} element={<AgreementPage/>} />
                 <Route path={'offer'} element={<OfferPage/>} />
-                <Route path={'refund'} element={<RefundPage/>} />
                 <Route path={'privacy_policy'} element={<PrivacyPage/>} />
+                <Route path={'refund'} element={<RefundPage/>} />
 
-                <Route path={'events/2406411'} element={<PostponementPage/>} />
                 <Route path={'*'} element={<NotFoundPage />} />
             </Routes>
         </Suspense>
---
+++ src/pages/EventsPage/EventsPage.tsx
@@ -0,0 +1,102 @@
+
+const EventsPage = () => {
+    const months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь', 'Январь', 'Февраль', 'Март', 'Апрель']
+    const [firstMonth, setFirstMonth] = useState(0)
+    const [activeMonthSection, setActiveMonthSection] = useState(0)
+    const [filteredEvents, setFilteredEvents] = useState<Event[]>([]);
+    const {selectedCity} = useCity();
+
+    const [events, setEvents] = useState<Event[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const xl = useMediaBreakpoint('xl')
+
+    useEffect(() => {
+        const fetchEvents = async () => {
+            try {
+                setLoading(true);
+                const eventsList = await DrupalAPI.getEvents();
+                setEvents(eventsList.filter((item) => item.city == selectedCity));
+                setFirstMonth(getDate(eventsList[0].date).monthNum - 1);
+
+            } catch (err) {
+                setError(err instanceof Error ? err.message : 'Unknown error');
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchEvents();
+    }, [selectedCity]);
+
+    useEffect(() => {
+        if (activeMonthSection === 0) {
+            setFilteredEvents(events);
+        } else {
+            const targetMonthIndex = firstMonth + activeMonthSection - 1;
+            const filtered = events.filter(event => {
+                const eventMonth = getDate(event.date).monthNum - 1;
+                return eventMonth === targetMonthIndex;
+            });
+            setFilteredEvents(filtered);
+        }
+    }, [activeMonthSection, events, firstMonth]);
+
+
+    if(loading) { return <LoadingSpinner/> }
+    if(error) { return <>{error}</> }
+
+    return (
+        <section id='eventlist' className='flex flex-col gap-10 bg-darkgray text-white w-[90vw] xl:w-[1166px]'>
+            <Text variant={TextVariant.H2}>АФИША {selectedCity?.toUpperCase()}</Text>
+            <div className='grid grid-cols-3 justify-items-center gap-2 md:grid-cols-4 xl:flex xl:gap-[30px]'>
+                <MonthButton
+                    isActive={activeMonthSection === 0}
+                    count={events.length}
+                    setActive={() => setActiveMonthSection(0)}
+                >
+                    Все даты
+                </MonthButton>
+                {months.map((month, index) => (
+                    (index >= firstMonth && index <= firstMonth + 4) &&
+                    <MonthButton
+                        key={month + index}
+                        setActive={() => setActiveMonthSection(index - firstMonth + 1)}
+                        isActive={activeMonthSection === index - firstMonth + 1}
+                        count={events.filter(event => {
+                            const eventMonth = getDate(event.date).monthNum - 1;
+                            return eventMonth === index;
+                        }).length}
+                    >
+                        {month}
+                    </MonthButton>
+                ))}
+            </div>
+            <div className='flex flex-col gap-[50px]'>
+                {filteredEvents.length > 0 ? (
+                    filteredEvents.map((item, index) => (
+                            <>
+                                {xl
+                                    ? <EventCardDesktop key={index} item={item} to={createSlug(item.eventId)}/>
+                                    : <EventCardMobile key={index} item={item} to={createSlug(item.eventId)}/>
+                                }
+                                {index !== filteredEvents.length - 1 && <div className='w-full border-solid border-semi-darkgray border-t-[2px] border-x-0 border-b-0'/>}
+
+                            </>
+
+                        )
+                    )
+                ) : (
+                    <Text className='text-lightgray' variant={TextVariant.Body_S}>
+                        {activeMonthSection === 0
+                            ? 'В ближайшее время концертов не планируется. Следите за обновлениями!'
+                            : `В этом месяце концертов не планируется. Следите за обновлениями!`
+                        }
+                    </Text>
+                )}
+            </div>
+        </section>
+    );
+};
+
+export default EventsPage;
