07d563e4f503af306c1a1c88afc78152f16a9adc
yanemasson <gluedboy@gmail.com>
2025-12-22 16:54:13 +0700

    Исправлена верстка CookieConsent для планшетов

--- src/components/CookieConsent.tsx
+++ src/components/CookieConsent.tsx
@@ -47,7 +47,7 @@ const CookieConsent = (
     };
 
     return (
-        <div className='bg-bg-island p-4 w-[360px] fixed bottom-0 z-50 md:right-5 md:bottom-5 shadow flex flex-col gap-3'>
+        <div className='bg-bg-island p-4 md:w-[360px] w-full fixed bottom-0 z-50 md:right-5 md:bottom-5 shadow flex flex-col gap-3'>
             <div className='text-text-primary flex justify-between'>
                 <Text variant={TextVariant.Subtitle_M}>Мы собираем cookie</Text>
                 <div className='cursor-pointer' onClick={handleAcceptAll}>
