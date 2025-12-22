dc5581ce2d98bbb5925948f039690831b980d401
yanemasson <gluedboy@gmail.com>
2025-12-22 16:51:16 +0700

    Обновлен CityConfirmationModal

--- src/components/CityConfirmationModal.tsx
+++ src/components/CityConfirmationModal.tsx
@@ -1,6 +1,6 @@
 import Text, {TextVariant} from "./Text.tsx";
-import Button, {ButtonVariant} from "./Buttons/Button.tsx";
 import {useNavigate} from "react-router";
+import Button, {ButtonSize, ButtonVariant} from "./Buttons/Button.tsx";
 
 interface CityConfirmationModalProps {
     city?: string | null
@@ -11,29 +11,32 @@ interface CityConfirmationModalProps {
 const CityConfirmationModal = ({ city, onConfirm, onChangeCity }:CityConfirmationModalProps) => {
 
     const navigate = useNavigate()
-    const handleClick = () => {
+    const handleConfirm = () => {
         onConfirm();
         navigate(0);
     }
 
     return (
-        <div className='fixed md:absolute left-0 md:top-8 w-full md:w-fit bg-semi-darkgray p-6 flex h-117 md:h-auto'>
-            <div className='flex flex-col gap-5 items-start md:items-center md:w-[255px]'>
-                {city
-                    ? <>
-                        <Text variant={TextVariant.P}>Ваш город {city}?</Text>
-                        <div className='flex gap-4 items-center'>
-                            <Button className='w-[40vw] md:w-20 h-8' onClick={onChangeCity} variant={ButtonVariant.outline}>Нет</Button>
-                            <Button className='w-[40vw] md:w-20 h-8' onClick={handleClick} variant={ButtonVariant.primary}>Да</Button>
-                        </div>
-                    </>
-                    : <>
-                        <Text variant={TextVariant.P}>Не удалось определить город</Text>
-                        <Button className='w-40 h-11' onClick={onChangeCity} variant={ButtonVariant.outline}>Выбрать город</Button>
-                    </>
-                }
+        <div className="fixed top-[76px] left-0 md:top-[92px] md:left-4 p-4 flex flex-col gap-4 w-full md:w-[360px] bg-bg-island">
+            <Text variant={TextVariant.Subtitle_M}>{`Ваш город - ${city}?`}</Text>
+            <div className='flex gap-3 '>
+                <Button
+                    className='flex-1'
+                    size={ButtonSize.small}
+                    variant={ButtonVariant.tertiary}
+                    onClick={() => onChangeCity()}
+                >
+                    Сменить город
+                </Button>
+                <Button
+                    className='flex-1'
+                    size={ButtonSize.small}
+                    variant={ButtonVariant.primary}
+                    onClick={() => handleConfirm()}
+                >
+                    Да, верно
+                </Button>
             </div>
-
         </div>
     );
 };
