bc158b158e69de9ae5eaa5beb0013e5d8f635650
yanemasson <gluedboy@gmail.com>
2026-01-15 16:08:03 +0700

    Исправлена верстка страниц offer, privacy, refund и agreement

--- src/pages/RefundPage/RefundPage.tsx
+++ src/pages/RefundPage/RefundPage.tsx
@@ -10,13 +10,13 @@ const RefundPage = () => {
             <SEO title={'Возврат билетов | Бэкстейдж, афиша, концерт, билеты'}
                  description={"Билеты на лучшие балетные спектакли и симфонические концерты." +
                      "Классическая музыка, премьеры в Вашем городе"}/>
-            <div className='flex flex-col w-full items-start gap-10 '>
+            <div className='flex flex-col w-[90vw] xl:w-[1152px] items-start gap-10 mt-[76px]'>
                 <div className=' flex flex-col items-start justify-center gap-5 pt-20 px-5 xl:px-0'>
                     <Text variant={TextVariant.H1}>Возврат билетов</Text>
-                    <Text variant={TextVariant.B}>Уважаемые зрители! Если Вы хотите вернуть билет, выберите, пожалуйста,
+                    <Text variant={TextVariant.Body_M}>Уважаемые зрители! Если Вы хотите вернуть билет, выберите, пожалуйста,
                         вашу ситуацию и дальше действуйте СОГЛАСНО ИНСТРУКЦИИ:
                     </Text>
-                    <Text variant={TextVariant.P}>
+                    <Text variant={TextVariant.Body_M}>
                         В соответствии с Федеральным законом от 18.07.2019 N 193-ФЗ
                         «О внесении изменений в Закон РФ «Основы законодательства РФ о культуре»
                         установлен следующий порядок возврата приобретенных билетов.
@@ -31,35 +31,35 @@ const RefundPage = () => {
                         при наличии документов подтверждающих факт болезни.
                         <br/>Сервисный сбор (если он взимался при приобретении билетов) не возвращается!
                     </Text>
-                    <Text variant={TextVariant.P}>
+                    <Text variant={TextVariant.Body_M}>
                         Если билеты  приобретались на нашем сайте https://backstagegroup.ru/ для возврата {' '}
                         <Anchor href={'https://radario.ru/customer/refunds/submit'}>
-                            <Text variant={TextVariant.B}>ВОСПОЛЬЗУЙТЕСЬ ФОРМОЙ ВОЗВРАТА</Text>
+                            ВОСПОЛЬЗУЙТЕСЬ ФОРМОЙ ВОЗВРАТА
                         </Anchor> уполномоченного билетного оператора «РАДАРИО»
                     </Text>
-                    <Text variant={TextVariant.P}>
+                    <Text variant={TextVariant.Body_M}>
                         Если билеты приобретались у билетного оператора «Kassy.ru», возврат осуществляется {' '}
                         <Anchor href={'https://kras.kassy.ru/text/pravila-vozvrata-biletov/'}>
-                            <Text variant={TextVariant.B}>согласно правилам возврата компании</Text>
+                            согласно правилам возврата компании
                         </Anchor>
                         <br/>Дополнительная информация по телефону +7 (391) 206-90-00
                     </Text>
-                    <Text variant={TextVariant.P}>
+                    <Text variant={TextVariant.Body_M}>
                         Если билеты приобретались у билетного оператора «Красбилет», возврат осуществляется {' '}
                         <Anchor href={'https://krasbilet.ru/text/pravila-vozvrata-biletov/'}>
-                            <Text variant={TextVariant.B}>согласно правилам возврата компании</Text>
+                            согласно правилам возврата компании
                         </Anchor>
                         <br/>Дополнительная информация по телефону +7 (391) 288-88-81
                     </Text>
-                    <Text variant={TextVariant.B}>
+                    <Text variant={TextVariant.Body_M}>
                         Если вы купили билет на нашем сайте, и он не пришел Вам на почту, напишите {' '}
                         <Anchor href={'mailto:help@radario.ru'}>help@radario.ru</Anchor>
                         <br/>Служба поддержки работает с 7:00 до 23:00 (МСК)
                     </Text>
                 </div>
-                <div className='flex items-center gap-5 '>
+                <div className='flex self-center xl:self-start '>
                     <Link to='/'>
-                        <Button className='w-[284px] h-[53px]' variant={ButtonVariant.outline}>Главная</Button>
+                        <Button className='w-[90vw] xl:w-[284px]' variant={ButtonVariant.secondary}>Главная</Button>
                     </Link>
                 </div>
             </div>
