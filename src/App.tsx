65a43b9e4eb708645aa92be5360e2daaf9dd60fb
yanemasson <gluedboy@gmail.com>
2026-01-21 17:29:54 +0700

    Cookie consent и cookie provider перенесены в App

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,30 @@ import ScrollToTop from "./components/ScrollToTop.tsx";
 import AppRoutes from "./components/AppRoutes.tsx";
 import MetricsProvider from "./components/Metrika/MetricsProvider.tsx";
 import {CityProvider} from "./context/CityContext.tsx";
+import {CookieProvider} from "./context/CookieContext.tsx";
+import CookieConsent from "./components/CookieConsent.tsx";
 
 function App() {
 
     return (
         <HelmetProvider>
             <CityProvider>
-                <BrowserRouter>
-                    <MetricsProvider/>
-                    <ScrollToTop />
-                    <div className='bg-bg-global text-text-primary flex flex-col justify-center items-center'>
-                        <div className="flex flex-col items-center min-h-screen ">
-                            <Navbar/>
-                            <main className="flex-grow flex flex-col items-center mb-20 w-full gap-24">
-                                <AppRoutes/>
-                                <Footer/>
-                            </main>
+                <CookieProvider>
+                    <BrowserRouter>
+                        <MetricsProvider/>
+                        <CookieConsent/>
+                        <ScrollToTop />
+                        <div className='bg-bg-global text-text-primary flex flex-col justify-center items-center'>
+                            <div className="flex flex-col items-center min-h-screen ">
+                                <Navbar/>
+                                <main className="flex-grow flex flex-col items-center mb-20 w-full gap-24">
+                                    <AppRoutes/>
+                                    <Footer/>
+                                </main>
+                            </div>
                         </div>
-                    </div>
-                </BrowserRouter>
+                    </BrowserRouter>
+                </CookieProvider>
             </CityProvider>
         </HelmetProvider>
   )
