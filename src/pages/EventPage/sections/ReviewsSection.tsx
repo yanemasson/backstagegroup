99e93bd2875110113f0aa88c76f913f818b770ed
yanemasson <gluedboy@gmail.com>
2026-01-25 13:08:21 +0700

    Обновлены фотографии в секции Reviews

--- src/pages/EventPage/sections/ReviewsSection.tsx
+++ src/pages/EventPage/sections/ReviewsSection.tsx
@@ -5,7 +5,7 @@ import ReviewComponent from "../../../components/ReviewComponent.tsx";
 
 const ReviewsSection = () => {
     const {reviews} = useReviews();
-    const arr = [1, 2, 3, 4, 5, 6]
+    const arr = [1, 2, 3, 4, 5]
 
     return (
         <section className='flex flex-col w-[90vw] md:w-[1166px] gap-[50px] md:gap-[60px] min-h-[254px] '>
@@ -16,8 +16,8 @@ const ReviewsSection = () => {
                 <div className='flex gap-2.5 overflow-x-auto pt-[30px]'>
                     {arr.map((item) => (
                         <img
-                            alt={'/images/review/' + item + '.png' }
-                            src={'/images/review/' + item + '.png' }
+                            alt={'/images/review/' + item + '.jpg' }
+                            src={'/images/review/' + item + '.jpg' }
                             className='md:w-[186px] md:h-[276px] w-[290px] h-[431px] object-center object-cover'
                         />
                     ))}
