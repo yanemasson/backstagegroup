ff9ebd18620cf95381f36e8f7a79ed536cef3a86
yanemasson <gluedboy@gmail.com>
2026-03-30 15:46:31 +0700

    Компонент Information обновлен: при отсутствии блоков с информацией отображает полное описание концерта вместо них

--- src/pages/EventPage/sections/Information.tsx
+++ src/pages/EventPage/sections/Information.tsx
@@ -1,19 +1,30 @@
 import Text, {TextVariant} from "../../../components/Text.tsx";
 import {InformationItem} from "../../../types/events/information_item.ts";
 
-const Information = ({information} : {information: InformationItem[]}) => {
+interface InformationProps {
+    information: InformationItem[];
+    descriptionFull: string;
+}
+
+const Information = ({information, descriptionFull} : InformationProps) => {
     return (
         <section className='flex flex-col gap-11' id='description'>
-            {information.map((item) => (
-                <div className='flex xl:flex-row flex-col gap-6' key={item.title}>
-                    <img className='xl:min-w-[667px] xl:max-h-[426px] min-w-[90vw] overflow-hidden' alt={item.photo} src={item.photo}/>
-                    <div className='flex flex-col gap-3 justify-center' >
-                        <Text variant={TextVariant.Subtitle_L} className='text-text-accent'>{item.title}</Text>
-                        <Text variant={TextVariant.Body_L} >{item.text}</Text>
+            {information[0]?.title
+                ? information.map((item) => (
+                    <div className='flex xl:flex-row flex-col gap-6' key={item.title}>
+                        <img
+                            className='xl:min-w-[667px] xl:max-h-[426px] min-w-[90vw] overflow-hidden'
+                            alt={item.photo}
+                            src={item.photo}
+                        />
+                        <div className='flex flex-col gap-3 justify-center' >
+                            <Text variant={TextVariant.Subtitle_L} className='text-text-accent'>{item.title}</Text>
+                            <Text variant={TextVariant.Body_L} >{item.text}</Text>
+                        </div>
                     </div>
-                </div>
-                )
-            )}
+                ))
+                : <Text variant={TextVariant.Body_M}>{descriptionFull}</Text>
+            }
         </section>
     );
 };
